e58e367e124547fad8e0ad1d34c4f42eb5e26d2a
nikhilsaisrikarn@gmail.com <nikhilsaisrikarn@gmail.com>
2026-07-15 21:19:45 +0530

    add: subject CRUD operations

--- controllers/subject.controller.js
+++ controllers/subject.controller.js
@@ -2,20 +2,57 @@ import ApiError from "../classes/apiError.class.js";
 import FacultyModel from "../models/faculty.model.js";
 import GroupModel from "../models/group.model.js";
 import SubjectModel from "../models/subject.model.js";
+import UserModel from "../models/user.model.js";
 
-export const getSubjectController = (req, res, next) => {
-    
+export const getSubjectController = async (req, res, next) => {
+    try {
+        const subjectId = req.params.subjectId;
+
+        const subject = await SubjectModel.findById(subjectId);
+
+        if(!subject) {
+            throw new ApiError(404, "Subject not found");
+        }
+
+        if(!subject.groupId.equals(req.user.groupId)) {
+            throw new ApiError(403, "Forbidden");
+        }
+
+        res.status(200).send({
+            success: true,
+            message: "Subject Found",
+            data: {
+                subject: subject
+            }
+        });
+    } catch(e) {
+        next(e);
+    }
 }
 
-export const getSubjectsController = (req, res, next) => {
+export const getSubjectsController = async (req, res, next) => {
+    try {
+        const subjects = await SubjectModel.find({ groupId: req.user.groupId });
 
+        res.status(200).send({
+            success: true,
+            message: "Subjects Found",
+            data: {
+                subjects: subjects
+            }
+        });
+    } catch(e) {
+        next(e);
+    }
 }
 
 export const createSubjectController = async (req, res, next) => {
     try {
-        const { name, facultyId, credits, moderatorId } = req.body;
+        const { name, facultyId, credits } = req.body;
 
-        if(!name || !credits || !moderatorId) {
+        const moderatorId = req.user._id;
+
+        if(!name || !credits) {
             throw new ApiError(400, "Missing required fields");
         }
 
@@ -30,11 +67,16 @@ export const createSubjectController = async (req, res, next) => {
             throw new ApiError(400, "You are not moderator of any Group");
         }
 
-        const subject = await SubjectModel.create({ name, facultyId, credits, groupId: group._id});
+        let subject = await SubjectModel.findOne({ name, groupId: group._id});
+        if(subject) {
+            throw new ApiError(400, "Subject already exists");
+        }
+
+        subject = await SubjectModel.create({ name, facultyId, credits, groupId: group._id});
 
         res.status(201).send({
             success: true,
-            message: "Subejct created successfully",
+            message: "Subject created successfully",
             data: {
                 subject: subject,
             }
@@ -44,10 +86,78 @@ export const createSubjectController = async (req, res, next) => {
     }
 }
 
-export const updateSubjectController = (req, res, next) => {
+export const updateSubjectController = async (req, res, next) => {
+    try {
+        const { name, facultyId, credits } = req.body;
 
+        let subject = await SubjectModel.findById(req.params.subjectId);
+
+        if(!subject) {
+            throw new ApiError(404, "No subject found");
+        }
+
+
+        if (!subject.groupId.equals(req.user.groupId)) {
+            throw new ApiError(403, "Forbidden");
+        }
+
+        if(facultyId && !(await FacultyModel.exists({ _id: facultyId }))) {
+            throw new ApiError(404, "Faculty does not exist");
+        }
+
+
+        if(facultyId) subject.facultyId = facultyId;
+        if(credits !== undefined) subject.credits = credits;
+        if (name) {
+            const exists = await SubjectModel.exists({
+                name,
+                groupId: subject.groupId,
+                _id: { $ne: subject._id }
+            });
+
+            if (exists) {
+                throw new ApiError(409, "Subject already exists");
+            }
+
+            subject.name = name;
+        }
+
+        await subject.save();
+
+        res.status(200).send({
+            success: true,
+            message: "Subject Updated successfully",
+            data: {
+                subject: subject,
+            }
+        })
+    } catch (e) {
+        next(e);
+    }
 }
 
-export const deleteSubjectController = (req, res, next) => {
+export const deleteSubjectController = async (req, res, next) => {
+    try {
+        const subject = await SubjectModel.findById(req.params.subjectId);
 
+        if(!subject) {
+            throw new ApiError(404, "No subject found");
+        }
+
+        if (!subject.groupId.equals(req.user.groupId)) {
+            throw new ApiError(403, "Forbidden");
+        }
+
+        await SubjectModel.findByIdAndDelete(req.params.subjectId);
+
+        res.status(200).send({
+            success: true,
+            message: "Subject deleted successfully",
+            data: {
+                subject: subject,
+            }
+        })
+    } catch (e) {
+        next(e);
+    }
 }
